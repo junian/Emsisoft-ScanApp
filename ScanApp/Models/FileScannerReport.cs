f9b313bd6a1aea979f0dcd1055c7ee2cf80c9c5f
Junian Triajianto <author@junian.net>
2022-07-05 15:57:40 +0700

    Add File scanner report

---
+++ ScanApp/Models/FileScannerReport.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ScanApp.Models
+{
+    public class FileScannerReport
+    {
+        public FileScannerReport()
+        {
+            TotalFiles = 0;
+            TotalErrors = 0;
+            FileHashList = new List<FileHash> {};
+        }
+
+        public long TotalFiles { get; set; }
+        public long TotalErrors { get; set; }
+
+        public IList<FileHash> FileHashList { get; set; }
+    }
+}
+
