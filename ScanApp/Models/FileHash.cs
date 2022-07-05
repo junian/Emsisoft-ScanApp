2152d40d05284b023115302f427c8110ef54d725
Junian Triajianto <author@junian.net>
2022-07-05 15:07:24 +0700

    Add FileHash model

---
+++ ScanApp/Models/FileHash.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace ScanApp.Models
+{
+    public class FileHash
+    {
+        public FileHash()
+        {
+        }
+
+        public string FilePath { get; set; }
+        public string MD5 { get; set; }
+        public string SHA1 { get; set; }
+        public string SHA256 { get; set; }
+
+    }
+}
