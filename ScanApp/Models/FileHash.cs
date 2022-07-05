c908225c6aa4573237c79d8d45cba521b39ad201
Junian Triajianto <author@junian.net>
2022-07-05 15:57:12 +0700

    Set error in filehash

--- ScanApp/Models/FileHash.cs
+++ ScanApp/Models/FileHash.cs
@@ -11,6 +11,7 @@ namespace ScanApp.Models
         public string MD5 { get; set; }
         public string SHA1 { get; set; }
         public string SHA256 { get; set; }
-
+        public bool IsError { get; set; }
+        public string ErrorMessage { get; set; }
     }
 }
